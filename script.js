972773ee7f9670a7515ecdc6076a179a1e2ee7e2
xweeton <137170109+xweeton@users.noreply.github.com>
2023-09-14 13:46:32 +0000

    Initial commit

--- script.js
+++ script.js
@@ -13,16 +13,18 @@ let score = 0;
 let pageNumber = 0;
 
 function submitAnswer() {
-   // Get the correct answer for the current page (question) and convert it to lowercase.
+
   const answerInput = document.getElementById('answerInput').value.toLowerCase();
 
+  // Get the correct answer for the current page, like question[0] will take the first question
   const correctAnswer = questions[pageNumber].answer.toLowerCase();
 
   if (answerInput === correctAnswer) {
     score++
   }
 
-pageNumber++;
+  // add pageNumber when clicked submitAnswer()
+  pageNumber++;
 
   // Check if there are more questions to display.
   if (pageNumber < questions.length) {
@@ -35,19 +37,26 @@ pageNumber++;
 function displayQuestion() {
   const questionText = document.getElementById('questionText');
 
-   // Set the text content of the element to the current question.
-  questionText.textContent = questions[pageNumber].question;
+  // Show question to webpage
+  questionText.innerHTML = questions[pageNumber].question;
 
-// Clear the answer input field for the next question
-document.getElementById('answerInput').value = '';
+  // Clear the answer input field for the next question
+  document.getElementById('answerInput').value = '';
 }
 
 function showScore() {
   const result = document.getElementById('result');
-  
+
   result.innerHTML = `Your score is ${score}/${questions.length}`;
 
+  // Hide the whole questionContainer after showScore()
   document.getElementById('questionContainer').style.display = 'none'
+}
+
+function checkEnter(event) {
+  if (event.keyCode === 13) {
+    submitAnswer();
+  }
 }
 
 displayQuestion();
